dcfa2c6c85bdc6d807d71799ea8523cf34ba6db6
ParthVazare <126350841+ParthVazaree@users.noreply.github.com>
2023-05-09 14:01:10 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,71 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBn0_1Gv0m5B9TyJALuN5eevvpDbTpuRJ4",
+  authDomain: "kwitter-dfd19.firebaseapp.com",
+  databaseURL: "https://kwitter-dfd19-default-rtdb.firebaseio.com",
+  projectId: "kwitter-dfd19",
+  storageBucket: "kwitter-dfd19.appspot.com",
+  messagingSenderId: "693456925223",
+  appId: "1:693456925223:web:950f2950e6e170d1785402",
+
+};
+
+
+
+  firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "index.html";
+}
+
+getData();
+
+function redirectToRoomName(name)
+{
+  console.log(name);
+  localStorage.setItem("room_name", name);
+    window.location = "kwitter_page.html";
+}
+
+function logout() {
+localStorage.removeItem("user_name");
+localStorage.removeItem("room_name");
+    window.location = "kwitter.html";
+}
